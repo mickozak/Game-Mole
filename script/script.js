8af94f47004cae5684b05657adb45d21e106aa87
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-25 17:41:34 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,19 @@
-
+(function(){
+   
+let mole;
+let time;
+let points;
+let gameIntervalID;
+    
+function addPoint(){
+    points++;
+    displayPoints(points);
+}
+    
+function reduceTime(){
+    time--;
+    displayTimes(time);
+    if(time===0){
+        endGame()
+    }
+}
