55cefb5a75110f994063bec2ef8ab9857f6e0716
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-25 17:56:10 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -27,3 +27,26 @@ function displayTimes(timeParam){
     let timeContainer = document.querySelector('.time');
     timeContainer.innerText='Your time: ' + time;
 }
+
+function makeMole(){
+    
+    let molePosX = Math.round(Math.random()*(window.innerWidth-window.innerHeight/10));
+    let molePosY = Math.round(Math.random()*(window.innerHeight-window.innerHeight/10));
+    
+    let mole = document.createElement('div');
+    mole.classList.add('mole');
+    
+    mole.style.left = molePosX + 'px';
+    mole.style.top = molePosY + 'px';
+    
+    mole.addEventListener('click',function(){
+        mole.remove();
+        addPoint();
+        flashBackground();
+    })
+    
+    document.querySelector('body').appendChild(mole);
+    
+    return mole;
+    
+} 
